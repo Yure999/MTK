a9ce520def780d7c78922dcc13d68a58d398e627
Martin Bueno Martin <56157505+Yure999@users.noreply.github.com>
2021-01-26 00:16:31 +0100

    implemented UI in Android app

--- androidApp/src/main/java/com/example/mejoratukmm/androidApp/MainActivity.kt
+++ androidApp/src/main/java/com/example/mejoratukmm/androidApp/MainActivity.kt
@@ -2,19 +2,30 @@ package com.example.mejoratukmm.androidApp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.example.mejoratukmm.shared.Greeting
 import android.widget.TextView
-
-fun greet(): String {
-    return Greeting().greeting()
-}
+import com.example.mejoratukmm.shared.diceRoller.DiceRoller
 
 class MainActivity : AppCompatActivity() {
+    private val diceRoller = DiceRoller()
+    private fun rollDice() {
+        diceRoller.rollDice()
+    }
+
+    private fun setText(): String {
+        return diceRoller.getDiceNumber().toString()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val tv: TextView = findViewById(R.id.text_view)
-        tv.text = greet()
+        val button: Button = findViewById(R.id.button)
+        button.setOnClickListener {
+            rollDice()
+            tv.text = setText()
+        }
     }
 }
